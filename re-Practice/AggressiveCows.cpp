b0c6ef50b4570868bb406d54a2fd25155bb51acc
jai-00 <jai25june@gmail.com>
2026-08-06 21:45:58 +0530

    Aggressive cows problem

---
+++ re-Practice/AggressiveCows.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums = {4, 1, 5, 2, 8, 9}
+
+    return 0;
+}
