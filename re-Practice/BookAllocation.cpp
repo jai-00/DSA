be26ad3f00ad107b916114531f436b0fa5530aca
jai-00 <jai25june@gmail.com>
2026-08-06 14:00:37 +0530

    Book Allocation problem solution

--- re-Practice/BookAllocation.cpp
+++ re-Practice/BookAllocation.cpp
@@ -4,8 +4,61 @@
 #include <climits>
 
 using namespace std;
+
+bool isValid(const vector<int> &nums, int maxAllowedPages, int numberOfStudents)
+{
+    int requiredNumberOfStudents = 1, pageAllocated = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        pageAllocated = pageAllocated + nums[i];
+        if (pageAllocated > maxAllowedPages)
+        {
+            requiredNumberOfStudents++;
+            pageAllocated = nums[i];
+        }
+
+        if (requiredNumberOfStudents > numberOfStudents)
+            return false;
+    }
+
+    return true;
+}
+
 int findMinimum(const vector<int> &nums, int numberOfStudents)
 {
+    int maxPossible = 0;
+    int minPossible = 0;
+
+    int i = 0, j = nums.size() - 1;
+
+    while (i <= j)
+    {
+        maxPossible += nums[i];
+        minPossible = max(minPossible, nums[i]);
+        i++;
+    }
+
+    i = minPossible;
+    j = maxPossible;
+    int ans = 0;
+
+    while (i <= j)
+    {
+        int mid = i + ((j - i) / 2);
+
+        if (isValid(nums, mid, numberOfStudents))
+        {
+            j = mid - 1;
+            ans = mid;
+        }
+        else
+        {
+            i = mid + 1;
+        }
+    }
+
+    return ans;
 }
 int main()
 {
@@ -13,7 +66,14 @@ int main()
     int k = 2;
     int minimumPossiblePages = findMinimum(nums, k);
     cout << "--------" << endl;
-    cout << "Lowest possible page for a single student : " << minimumPossiblePages << endl;
+    if (k > nums.size())
+    {
+        cout << "Answer not Possible" << endl;
+    }
+    else
+    {
+        cout << "Lowest possible maximum page for a single student : " << minimumPossiblePages << endl;
+    }
     cout << "--------" << endl;
     return 0;
 }
